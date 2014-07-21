37c22844b591e3d903d37a7d63ff6b3f67b3ba30
wenLiangcan <boxeed@gmail.com>
2014-07-21 13:58:27 +0800

    Add screenrc

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 dir=${HOME}/dotfiles
 bkdir=${HOME}/dotfiles_bk
-files="autoenv linuxbrew npmrc oh-my-zsh pip tty.js zshrc"
+files="autoenv linuxbrew npmrc oh-my-zsh pip screenrc tty.js zshrc"
 
 echo "Creating ${bkdir} for backup of any existing dotfiles in ~"
 mkdir -p ${bkdir}
